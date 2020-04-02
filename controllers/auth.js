b6e1c4d216a05192c0c300ab8d653948cb91da2d
iamkunall <103kunalsharma@gmail.com>
2020-04-02 21:25:53 +0530

    authentication added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,18 @@
 const User = require('../models/user');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 exports.signup = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+      params: errors.array()[0].param,
+    });
+  }
+
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
@@ -13,6 +25,40 @@ exports.signup = (req, res) => {
       email: user.email,
       id: user._id,
     });
+  });
+};
+
+exports.signin = (req, res) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: 'User email dose not find',
+      });
+    }
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: 'email is and password do not match',
+      });
+    }
+
+    //create token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+
+    //put token in cookie
+    res.cookie('token', token, { expire: new Date() + 9999 });
+
+    //response to front end
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, name, email, role } });
   });
 };
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const { check } = require('express-validator');
 
-const { signout, signup } = require('../controllers/auth');
+const { signout, signup, signin } = require('../controllers/auth');
 
-router.post('/signup', signup);
+router.post(
+  '/signup',
+  [
+    check('name', 'name is should be 3 char').isLength({ min: 5 }),
+    check('email', 'email is required').isEmail(),
+    check('password', 'password  should be 3 char').isLength({ min: 5 }),
+  ],
+  signup,
+);
 router.get('/signout', signout);
+
+router.post(
+  '/signin',
+  [
+    check('email', 'email is required').isEmail(),
+    check('password', 'password  should be required').isLength({ min: 1 }),
+  ],
+  signin,
+);
+
+router.get('/signin', signin);
 
 module.exports = router;
