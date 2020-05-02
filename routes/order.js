64074a3eff7d6b0b000e34c1ecbf7a4d586f0c7a
iamkunall <103kunalsharma@gmail,com>
2020-05-02 15:15:54 +0530

    order routes added

---
+++ routes/order.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+
+const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth');
+const { getUserByID, pushOrderInPurchaseList } = require('../controllers/user');
+const { updateStock } = require('../controllers/product');
+
+const {
+  getOrderById,
+  createOrder,
+  getAllOrder,
+  getOrderStatus,
+  updateStatus,
+} = require('../controllers/order');
+
+//params
+
+router.param('userId', getUserByID);
+router.param('orderId', getOrderById);
+
+//Actual routes
+router.post(
+  '/order/create/:userId',
+  isSignedIn,
+  isAuthenticated,
+  pushOrderInPurchaseList,
+  updateStock,
+  createOrder,
+);
+
+//read
+
+router.get(
+  '/order/all/:userId',
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  getAllOrder,
+);
+
+//status of order
+
+router.get(
+  '/order/status/:userId',
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  getOrderStatus,
+);
+router.put(
+  '/order/:orderId/status/:userId',
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateStatus,
+);
+
+module.exports = router;
